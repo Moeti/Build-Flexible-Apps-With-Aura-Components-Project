4948cde0bb1abb40bc5f40ae04cbe38b943fe2a0
MoetiK <moeti.khetsi@gmail.com>
2022-06-20 09:46:12 +0200

    Added Comments

--- force-app/main/default/aura/SimilarProperties/SimilarPropertiesController.js
+++ force-app/main/default/aura/SimilarProperties/SimilarPropertiesController.js
@@ -1,8 +1,9 @@
 ({
-    //Show Spinner
+    //Load Properties
     doInit : function(component, event, helper) {
         var spinner = component.find("spinner");
 		$A.util.removeClass(spinner, "slds-hide");
+        // Create action
         var action = component.get("c.getSimilarProperties");
         action.setParams({
             recordId: component.get("v.recordId"),
@@ -11,11 +12,13 @@
             searchCriteria: component.get("v.searchCriteria"),
             priceRange: parseInt(component.get("v.priceRange"), 10)
         });
+        // Add callback behavior for when response is received
         action.setCallback(this, function(response){
             var similarProperties = response.getReturnValue();
             component.set("v.similarProperties", similarProperties);
             $A.util.addClass(spinner, "slds-hide");
         });
+        // Send action off to be executed
         $A.enqueueAction(action);
     }
 })
